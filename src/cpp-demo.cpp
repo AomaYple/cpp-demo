8c8f4e67ef52a3de134f78af025be2f1103f2f23
AomaYple <2890599214@qq.com>
2025-11-30 17:16:57 +0800

    改进hello函数

--- src/cpp-demo.cpp
+++ src/cpp-demo.cpp
@@ -4,4 +4,4 @@
 
 using namespace std::string_view_literals;
 
-auto cpp_demo::hello() -> void { std::println("Hello cpp-demo!"sv); }
+auto cpp_demo::hello() -> void { std::println("Hello, cpp-demo!"sv); }
