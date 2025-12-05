6c2fcb2f642bad6cf44ad8c4b7fd1578e30c246d
AomaYple <2890599214@qq.com>
2025-12-05 23:18:26 +0800

    继续改进

--- include/cpp-demo.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace cpp_demo {
-    auto hello() -> void;
-}    // namespace cpp_demo
--- src/cpp-demo.cpp
+++
@@ -1,7 +0,0 @@
-#include "cpp-demo.hpp"
-
-#include <print>
-
-using namespace std::string_view_literals;
-
-auto cpp_demo::hello() -> void { std::println("Hello, cpp-demo!"sv); }
